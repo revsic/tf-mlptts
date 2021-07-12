53c4b61ac9ec3a842dea83fa5cee4e373d59f3dc
revsic <revsic99@gmail.com>
2021-07-13 00:45:25 +0900

    config: Add default config

--- mlptts/config.py
+++ mlptts/config.py
@@ -13,28 +13,29 @@ class Config:
         self.embedding = 256
         self.prenet_kernels = 3
 
-        self.channels = 128                         # 768
+        self.channels = 128  # 768
 
-        self.text_layers = 8                        # 12
-        self.text_ch_hiddens = self.channels * 4
-        self.text_kernels = 64                      # 192
-        self.text_strides = self.text_kernels // 4
-        self.text_tp_hiddens = 128                  # 384
-        self.text_dropout = 0.3                     # 0.
+        self.text_layers = 8  # 12
+        self.text_ch_hiddens = self.channels * 2  # factor 4
+        self.text_kernels = 192
+        self.text_strides = self.text_kernels // 2  # desired factor 4
+        self.text_tp_hiddens = 384
+        self.text_dropout = 0.
 
         self.res_layers = 4
         self.res_channels = 16
 
         self.dur_kernels = [5, 3, 1]
-        self.dur_dropout = 0.3                      # 0.
+        self.dur_dropout = 0.3
 
         self.reg_conv = 8
         self.reg_kernels = 3
         self.reg_mlp = 16
+        self.reg_aux = 2
 
-        self.mel_layers = 8                         # 12
-        self.mel_ch_hiddens = self.channels * 4
-        self.mel_kernels = 64                       # 192
-        self.mel_strides = self.mel_kernels // 4
-        self.mel_tp_hiddens = 128                   # 384
-        self.mel_dropout = 0.3                      # 0.
+        self.mel_layers = 8
+        self.mel_ch_hiddens = self.channels * 2
+        self.mel_kernels = 192
+        self.mel_strides = self.mel_kernels // 2
+        self.mel_tp_hiddens = 384
+        self.mel_dropout = 0.
