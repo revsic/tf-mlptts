0649e9cc8d596b8000e41e700d14245fa95794f5
revsic <revsic99@gmail.com>
2021-07-14 01:16:14 +0900

    init: Rename parameters

--- mlptts/mlpmixer/__init__.py
+++ mlptts/mlpmixer/__init__.py
@@ -9,21 +9,21 @@ class MLPMixer(tf.keras.Model):
     def __init__(self,
                  numlayers: int,
                  channels: int,
-                 ch_hiddens: int,
+                 hiddens: int,
                  eps: float,
                  dropout: float = 0.):
         """Initializer.
         Args:
             numlayers: the number of the mixer layers.
             channels: size of the input channels.
-            ch_hiddens: size of the hidden channels.
+            hiddens: size of the hidden channels.
             eps: small value for layer scale in dyn-temporal mlp.
             dropout: dropout rate.
         """
         super().__init__()
         self.blocks = tf.keras.Sequential([
             tf.keras.Sequential([
-                ChannelMLP(channels, ch_hiddens, dropout),
+                ChannelMLP(channels, hiddens, dropout),
                 DynTemporalMLP(eps, dropout)])
             for _ in range(numlayers)])
 
